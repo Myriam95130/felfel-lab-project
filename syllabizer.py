17462239391899cfefb67d0f41eb6b8c8747c43c
Myriam95130 <myriam95130@gmail.com>
2026-03-29 11:33:14 +0200

    ajout pour gestion des digrammes vocaliques

--- syllabizer.py
+++ syllabizer.py
@@ -1,6 +1,7 @@
 from arabizitns import dico_arabizi
 
-voyelles = ['a', 'e', 'i', 'y', 'o', 'ou', 'u']
+voyelles = ['a', 'e', 'i', 'y', 'o', 'u']
+digrammes_vocaliques = ['ou']
 consonnes = []
 
 for c in dico_arabizi.keys():
@@ -11,9 +12,15 @@ def phono_tns(mot_arabizi):
     i = 0
     structure = ""
     while i < len(mot_arabizi):
-        if mot_arabizi[i] in consonnes:
+        if mot_arabizi[i:i+2] in consonnes:
             structure += "C"
-            i += 1
+            i += 2
+        elif mot_arabizi[i] in consonnes:
+            structure += "C"
+            i += 1 
+        elif mot_arabizi[i:i+2] in digrammes_vocaliques:
+            structure += "V"
+            i += 2 
         elif mot_arabizi[i] in voyelles:
             structure += "V"
             i += 1
@@ -21,4 +28,4 @@ def phono_tns(mot_arabizi):
             i += 1
     return structure
     
-print(phono_tns("yesta3mel"))
+print(phono_tns("khouya"))
